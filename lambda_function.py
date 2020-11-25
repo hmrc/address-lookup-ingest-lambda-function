ba71b0a3dc384e93cc01d4bb19df5fd0eb06fecd
Saqib Saleem <saqib.saleem1@digital.hmrc.gov.uk>
2020-11-25 10:24:45 +0000

    [PS|SS] TAV-141: Fixed cleanup step to remove old entries from the address_lookup_status table.

--- lambda_function.py
+++ lambda_function.py
@@ -10,18 +10,31 @@ from psycopg2 import sql
 from split_abp_files import createCSV
 
 
-# batch_path is the full path to the batch directory, eg <root>/abp/79/2
 def process_handler(batch_info, context):
-    batch_dir = batch_info['batchDir']
+    """
+    Collects same types of data into same files across all downloaded zip files.
+    :param batch_info: json structure that holds path to directory containing files to process, eg <root>/abp/79/2
+    """
 
+    batch_dir = batch_info['batchDir']
     process_files(batch_dir)
 
 
 def create_schema_handler(epoch, context):
+    """
+    Creates a new schema for ingestion of new data.
+    :param epoch: the 'version' of data, eg 79
+    """
+
     return create_schema(epoch)
 
 
 def ingest_handler(batch_info, context):
+    """
+    Imports processed data into database using the COPY instruction.
+    :param batch_info: json structure that holds name of schema for data and the directory from which to import data
+    """
+
     db_schema_name = batch_info['schemaName']
     batch_dir = batch_info['batchDir']
 
@@ -29,16 +42,26 @@ def ingest_handler(batch_info, context):
 
 
 def create_lookup_view_and_indexes_handler(db_schema_name, context):
+    """
+    Creates view that brings tables together to make querying easier for address lookup purposes.
+    :param db_schema_name: schema where tables reside and where the view will be created.
+    """
+
     create_lookup_view_and_indexes(db_schema_name)
 
 
 # noinspection SqlResolve
 def check_status_handler(db_schema_name, context):
+    """
+    Checks whether the lookup view has been created
+    :param db_schema_name: schema where view is to be created
+    """
+
     with epoch_schema_connection(db_schema_name) as epoch_schema_con:
         with epoch_schema_con.cursor() as cur:
             cur.execute("""SELECT status FROM public.address_lookup_status WHERE schema_name = %s""",
                         (db_schema_name,))
-            status = cur.fetchone()[0] #If not rows found then error will be raised
+            status = cur.fetchone()[0]  # If not rows found then error will be raised
 
     epoch_schema_con.close()
 
@@ -93,14 +116,16 @@ def create_lookup_view_and_indexes(db_schema_name):
         with epoch_schema_con.cursor() as cur:
             cur.execute(lookup_view_sql)
     except Exception, e:
-        print 'There was a warning.  This is the info we have about it: %s' %(e)
+        print 'There was a warning.  This is the info we have about it: %s' % (e)
 
 
 def clean_status_table():
     with default_connection() as default_con:
         with default_con.cursor() as cur:
             def drop_schema(schema_to_drop):
-                cur.execute(sql.SQL("DROP SCHEMA IF EXISTS {} CASCADE".format(schema_to_drop)))
+                sql_to_execute = """DROP SCHEMA IF EXISTS {} CASCADE; 
+                    DELETE FROM public.address_lookup_status WHERE schema_name = '{}';"""
+                cur.execute(sql.SQL(sql_to_execute.format(schema_to_drop, schema_to_drop)))
 
             schemas_to_drop = get_schemas_to_drop(cur)
             map(drop_schema, schemas_to_drop)
@@ -112,15 +137,15 @@ def clean_status_table():
 # noinspection SqlResolve
 def get_schemas_to_drop(db_cur):
     db_cur.execute(
-     """SELECT schema_name
-        FROM public.address_lookup_status
-        WHERE schema_name NOT IN (
-            SELECT schema_name
-            FROM public.address_lookup_status
-            WHERE status = 'completed'
-            ORDER BY timestamp DESC
-            LIMIT 1
-        );""")
+        """SELECT schema_name
+           FROM public.address_lookup_status
+           WHERE schema_name NOT IN (
+               SELECT schema_name
+               FROM public.address_lookup_status
+               WHERE status = 'completed'
+               ORDER BY timestamp DESC
+               LIMIT 1
+           );""")
 
     schemas_to_drop = db_cur.fetchall()
     return map(lambda st: st[0], schemas_to_drop)
@@ -172,12 +197,12 @@ def wait(conn):
 def create_async_connection(options):
     con_params = db_con_params(options,
                                getSecret('address_lookup_rds_password',
-                                                  context={'role': 'address_lookup_file_download'}),
+                                         context={'role': 'address_lookup_file_download'}),
                                getSecret('address_lookup_db_host',
                                          context={'role': 'address_lookup_file_download'})
                                )
     conn = psycopg2.connect(host=con_params['host'], port=con_params['port'], database=con_params['database'], user=con_params['user'],
-                               password=con_params['password'], async=1, options=con_params['options'])
+                            password=con_params['password'], async=1, options=con_params['options'])
     wait(conn)
     return conn
 
@@ -185,7 +210,7 @@ def create_async_connection(options):
 def create_connection(options):
     con_params = db_con_params(options,
                                getSecret('address_lookup_rds_password',
-                                                  context={'role': 'address_lookup_file_download'}),
+                                         context={'role': 'address_lookup_file_download'}),
                                getSecret('address_lookup_db_host',
                                          context={'role': 'address_lookup_file_download'})
                                )
@@ -204,9 +229,9 @@ def db_con_params(options, password, host):
         "host": host,
         "port": 5432,
         "database": "addressbasepremium",
-        "user": "root",
+        "user"    : "root",
         "password": password,
-        "options": options
+        "options" : options
     }
 
 
