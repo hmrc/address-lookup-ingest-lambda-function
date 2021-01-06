589d8736ad291596ebeb9957f55aa443d5f32f89
Saqib Saleem <saqib.saleem1@digital.hmrc.gov.uk>
2021-01-06 16:48:41 +0000

    [PS|SS] TAV-191: Fixed the create statement to be a create followed by an alter.

--- create_status_table.sql
+++ create_status_table.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS public.address_lookup_status (
     schema_name VARCHAR(64) NOT NULL PRIMARY KEY,
     status      VARCHAR(32) NOT NULL,
-    error_message VARCHAR NULL,
     timestamp   TIMESTAMP NOT NULL
 );
+
+ALTER TABLE public.address_lookup_status
+ADD COLUMN error_message VARCHAR NULL;
