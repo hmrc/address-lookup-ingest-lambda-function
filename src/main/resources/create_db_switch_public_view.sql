f1015902787305a268d5e9d6866a09c9582a3572
ssaleem-ee <saqib.saleem1@digital.hmrc.gov.uk>
2021-12-29 06:52:45 +0000

    TAV-417: Add organisation_name, parent_uprn and usrn to the addresslookup view. Add duplicate columns with new and old name so that ALS is'nt broken for a short period of time. Bump sbt to 1.6.1 (more log4j related updates)

--- src/main/resources/create_db_switch_public_view.sql
+++ src/main/resources/create_db_switch_public_view.sql
@@ -1,4 +1,30 @@
 DROP VIEW IF EXISTS public.address_lookup;
-CREATE OR REPLACE VIEW public.address_lookup AS SELECT * FROM __schema__.address_lookup;
+CREATE OR REPLACE VIEW public.address_lookup AS
+SELECT uprn,
+       parent_uprn,
+       usrn,
+       organisation_name,
+       line1,
+       line2,
+       line3,
+       subdivision,
+       country_code,
+       country_code                 AS countrycode,
+       local_custodian_code,
+       local_custodian_code         AS localcustodiancode,
+       language,
+       blpu_state,
+       blpu_state                   AS blpustate,
+       logical_status,
+       logical_status               AS logicalstatus,
+       posttown,
+       postcode,
+       location,
+       pobox_number,
+       pobox_number                 AS poboxnumber,
+       local_authority,
+       local_authority              AS localAuthority,
+       address_lookup_ft_col
+FROM __schema__.address_lookup;
 GRANT SELECT ON public.address_lookup TO addresslookupreader;
 UPDATE public.address_lookup_status SET status = 'finalised' WHERE schema_name = '__schema__';
