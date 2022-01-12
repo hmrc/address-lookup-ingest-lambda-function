0808810f411aaa81659ed53d20e48e49ae969479
Pete Smith <pete@beyond-code.com>
2022-01-12 20:30:45 +1100

    [PS] TAV-489 - Add address_lookup_uppercase view to use as a base for the materialised view to ensure casing on text fields

--- src/main/resources/create_db_lookup_view_and_indexes_function.sql
+++ src/main/resources/create_db_lookup_view_and_indexes_function.sql
@@ -12,75 +12,106 @@ BEGIN
         timestamp = now()
     WHERE schema_name = the_schema_name;
 
-    DROP MATERIALIZED VIEW IF EXISTS address_lookup;
+    DROP VIEW IF EXISTS address_lookup_uppercase;
 
-    CREATE MATERIALIZED VIEW address_lookup AS
-    SELECT b.uprn                                                                                              AS uprn,
-           b.parent_uprn                                                                                       AS parent_uprn,
-           asd.usrn                                                                                            AS usrn,
-           NULLIF(BTRIM(d.organisation_name::text), '')                                                        AS organisation_name,
-           CASE WHEN NULLIF(BTRIM(d.po_box_number::text), '') IS NOT NULL THEN
-                    'PO BOX ' || BTRIM(d.po_box_number::text)
-                ELSE
-                    array_to_string(ARRAY [NULLIF(btrim(d.sub_building_name::text), ''), NULLIF(btrim(d.building_name::text), '')], ', '::text)
-               END                                                                                             AS line1,
-           CASE WHEN NULLIF(BTRIM(d.po_box_number::text), '') IS NOT NULL THEN ''
-                ELSE
-                    array_to_string(
-                        ARRAY [NULLIF(btrim(''::text || d.building_number), ''), NULLIF(btrim(d.dependent_thoroughfare::text), ''), NULLIF(btrim(d.thoroughfare::text), '')],
-                       ' '::text)
-               END                                                                                             AS line2,
-           array_to_string(ARRAY [NULLIF(btrim(d.double_dependent_locality::text), ''), NULLIF(btrim(d.dependent_locality::text), '')],
-                           ' '::text)                                                                          AS line3,
-           CASE
-               WHEN b.country::text = 'S'::text THEN 'GB-SCT'::text
-               WHEN b.country::text = 'E'::text THEN 'GB-ENG'::text
-               WHEN b.country::text = 'W'::text THEN 'GB-WLS'::text
-               WHEN b.country::text = 'N'::text THEN 'GB-NIR'::text
-               ELSE NULL::text
-               END                                                                                             AS subdivision,
-           CASE
-               WHEN b.country::text = 'S'::text THEN 'GB'::text
-               WHEN b.country::text = 'E'::text THEN 'GB'::text
-               WHEN b.country::text = 'W'::text THEN 'GB'::text
-               WHEN b.country::text = 'N'::text THEN 'GB'::text
-               WHEN b.country::text = 'L'::text AND "substring"(d.postcode::text, 1, 1) = 'G'::text THEN 'GG'::text
-               WHEN b.country::text = 'L'::text AND "substring"(d.postcode::text, 1, 1) = 'J'::text THEN 'JE'::text
-               WHEN b.country::text = 'M'::text THEN 'IM'::text
-               ELSE 'NOT_FOUND'::text
-               END                                                                                             AS country_code,
-           b.local_custodian_code                                                                              AS local_custodian_code,
-           CASE
-               WHEN lower(l.language::text) = 'eng'::text THEN 'en'::text
-               WHEN lower(l.language::text) = 'cym'::text THEN 'cy'::text
-               ELSE NULL::text
-               END                                                                                             AS language,
-           b.blpu_state                                                                                        AS blpu_state,
-           b.logical_status                                                                                    AS logical_status,
-           d.post_town                                                                                         AS posttown,
-           upper(
-                   regexp_replace(d.postcode::text, '[ \\t]+'::text, ' '::text))                               AS postcode,
-           concat(b.latitude, ',', b.longitude)                                                                AS location,
-           NULLIF(BTRIM(d.po_box_number::text), '')                                                            AS pobox_number,
-           asd.administrative_area                                                                             AS local_authority,
-           to_tsvector('english'::regconfig, array_to_string(
-                   ARRAY [
-                       NULLIF(btrim(d.sub_building_name::text), ''),
-                       NULLIF(btrim(d.building_name::text), ''),
-                       NULLIF(btrim(d.building_number::text), ''),
-                       NULLIF(btrim(d.dependent_thoroughfare::text), ''),
-                       NULLIF(btrim(d.thoroughfare::text), ''),
-                       NULLIF(btrim(d.post_town::text), ''),
-                       NULLIF(btrim(d.double_dependent_locality::text), ''),
-                       NULLIF(btrim(d.dependent_locality::text), ''),
-                       NULLIF(btrim(asd.administrative_area::text), ''),
-                       NULLIF(btrim(d.po_box_number::text), '')],
-                   ' '::text))                                                                                 AS address_lookup_ft_col
+    CREATE VIEW address_lookup_uppercase AS
+    SELECT b.uprn                             AS uprn,
+           b.parent_uprn                      AS parent_uprn,
+           asd.usrn                           AS usrn,
+           upper(d.organisation_name)         AS organisation_name,
+           upper(d.po_box_number)             AS po_box_number,
+           upper(d.sub_building_name)         AS sub_building_name,
+           upper(d.building_name)             AS building_name,
+           d.building_number                  AS building_number,
+           upper(d.dependent_thoroughfare)    AS dependent_thoroughfare,
+           upper(d.thoroughfare)              AS thoroughfare,
+           upper(d.double_dependent_locality) AS double_dependent_locality,
+           upper(d.dependent_locality)        AS dependent_locality,
+           upper(b.country)                   AS country,
+           b.local_custodian_code             AS local_custodian_code,
+           upper(l.language)                  AS "language",
+           upper(d.postcode)                  AS postcode,
+           b.blpu_state                       AS blpu_state,
+           b.logical_status                   AS logical_status,
+           upper(d.post_town)                 AS post_town,
+           b.latitude                         AS latitude,
+           b.longitude                        AS longitude,
+           asd.administrative_area            AS administrative_area
     FROM abp_delivery_point d
              JOIN abp_blpu b ON b.uprn = d.uprn
              JOIN abp_lpi l ON l.uprn = b.uprn AND l.logical_status = 1
              JOIN abp_street_descriptor asd ON l.usrn = asd.usrn and l.language = asd.language
-    WHERE    l.language = 'ENG';
+
+    DROP MATERIALIZED VIEW IF EXISTS address_lookup;
+
+    CREATE MATERIALIZED VIEW address_lookup AS
+    SELECT uprn                                                                                              AS uprn,
+           parent_uprn                                                                                       AS parent_uprn,
+           usrn                                                                                              AS usrn,
+           organisation_name                                                                                 AS organisation_name,
+           CASE
+               WHEN NULLIF(BTRIM(po_box_number::text), '') IS NOT NULL THEN
+                   'PO BOX ' || BTRIM(po_box_number::text)
+               ELSE
+                   array_to_string(ARRAY[NULLIF(btrim(sub_building_name::text), ''),
+                                   NULLIF(btrim(building_name::text), '') ], ', '::text)
+           END                                                                                               AS line1,
+           CASE
+               WHEN NULLIF(BTRIM(po_box_number::text), '') IS NOT NULL THEN ''
+               ELSE
+                   array_to_string(
+                           ARRAY[NULLIF(btrim(''::text || building_number), ''),
+                           NULLIF(btrim(dependent_thoroughfare::text), ''), NULLIF(btrim(thoroughfare::text), '') ],
+                           ' '::text)
+           END                                                                                               AS line2,
+           array_to_string(ARRAY[NULLIF(btrim(double_dependent_locality::text), ''),
+                           NULLIF(btrim(dependent_locality::text), '') ],
+                           ' '::text) AS line3,
+           CASE
+               WHEN country::text = 'S'::text THEN 'GB-SCT'::text
+               WHEN country::text = 'E'::text THEN 'GB-ENG'::text
+               WHEN country::text = 'W'::text THEN 'GB-WLS'::text
+               WHEN country::text = 'N'::text THEN 'GB-NIR'::text
+               ELSE NULL::text
+           END                                                                                               AS subdivision,
+           CASE
+               WHEN country::text = 'S'::text THEN 'GB'::text
+               WHEN country::text = 'E'::text THEN 'GB'::text
+               WHEN country::text = 'W'::text THEN 'GB'::text
+               WHEN country::text = 'N'::text THEN 'GB'::text
+               WHEN country::text = 'L'::text AND "substring"(postcode::text, 1, 1) = 'G'::text THEN 'GG'::text
+               WHEN country::text = 'L'::text AND "substring"(postcode::text, 1, 1) = 'J'::text THEN 'JE'::text
+               WHEN country::text = 'M'::text THEN 'IM'::text
+               ELSE 'NOT_FOUND'::text
+           END                                                                                                   AS country_code,
+           local_custodian_code                                                                                  AS local_custodian_code,
+           CASE
+               WHEN "language"::text = 'ENG'::text THEN 'en'::text
+               WHEN "language"::text = 'CYM'::text THEN 'cy'::text
+               ELSE NULL::text
+           END                                                                                                   AS "language",
+           blpu_state                                                                                            AS blpu_state,
+           logical_status                                                                                        AS logical_status,
+           post_town                                                                                             AS posttown,
+           upper(regexp_replace(postcode::text, '[ \\t]+'::text, ' '::text))                                     AS postcode,
+           concat(latitude, ',', longitude)                                                                      AS location,
+           NULLIF(BTRIM(po_box_number::text), '')                                                                AS pobox_number,
+           administrative_area                                                                                   AS local_authority,
+           to_tsvector('english'::regconfig, array_to_string(
+                   ARRAY [
+                       NULLIF(btrim(sub_building_name::text), ''),
+                       NULLIF(btrim(building_name::text), ''),
+                       NULLIF(btrim(building_number::text), ''),
+                       NULLIF(btrim(dependent_thoroughfare::text), ''),
+                       NULLIF(btrim(thoroughfare::text), ''),
+                       NULLIF(btrim(post_town::text), ''),
+                       NULLIF(btrim(double_dependent_locality::text), ''),
+                       NULLIF(btrim(dependent_locality::text), ''),
+                       NULLIF(btrim(administrative_area::text), ''),
+                       NULLIF(btrim(po_box_number::text), '')],
+                   ' '::text))                                                                                   AS address_lookup_ft_col
+    FROM address_lookup_uppercase
+    WHERE "language" = 'ENG';
 
     UPDATE public.address_lookup_status
     SET status    = 'view_created',
